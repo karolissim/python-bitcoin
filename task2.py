07b3b8d9d8378e346d95393ba9dc9fcff2250fe7
Karolis Simanavičius <56815036+knuckl35@users.noreply.github.com>
2020-12-03 16:59:11 +0200

    finish second task

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 from bitcoin.rpc import RawProxy
-import hashlib
+from hashlib import sha256
 import binascii
 import struct
 
@@ -9,32 +9,39 @@ def hexify(value, type):
 p = RawProxy()
 
 blockheight = 100000
+nextblockheight = blockheight + 1
 
+#get block
 blockhash = p.getblockhash(blockheight)
-
 block = p.getblock(blockhash)
 
 #block header data
-block_version = block['versionHex']
+block_version = block['version']
 previous_hash = block['previousblockhash']
-block_merkle_root = block['merkleroot']
+hash_merkle_root = block['merkleroot']
 bits = block['bits']
-timestamp = hex(block['time'])[-8:]
-nonce = hex(block['nonce'])[-8:]
+timestamp = block['time']
+nonce = block['nonce']
 
-header_hex = '{block_version}{previous_hash}{block_merkle_root}{timestamp}{bits}{nonce}'.format(
-        block_version = binascii.hexlify(block_version.decode('hex')[::-1]),
-        previous_hash = binascii.hexlify(previous_hash.decode('hex')[::-1]),
-        block_merkle_root = binascii.hexlify(block_merkle_root.decode('hex')[::-1]),
-        timestamp = binascii.hexlify(timestamp.decode('hex')[::-1]),
-        bits = binascii.hexlify(timestamp.decode('hex')[::-1]),
-        nonce = binascii.hexlify(nonce.decode('hex')[::-1])
+#calc header hex
+header_hex = '{block_version}{previous_hash}{hash_merkle_root}{timestamp}{bits}{nonce}'.format(
+    block_version=hexify(block_version, '<L'),
+    previous_hash=binascii.hexlify(previous_hash.decode('hex')[::-1]),
+    hash_merkle_root=binascii.hexlify(hash_merkle_root.decode('hex')[::-1]),
+    timestamp=hexify(timestamp, '<L'),
+    bits=binascii.hexlify(bits.decode('hex')[::-1]),
+    nonce=hexify(nonce, '<L')
 )
 
-#headerHex = blockVersion + prevBlock + blockMerkleHash + blockBits + blockTime + blockNonce
-#headerBin = headerHex.decode('hex')
-#hash = hashlib.sha256(hashlib.sha256(headerBin).digest()).digest()
-#hash.encode('hex_codec')
-#hash[::-1].encode('hex_codec')
+#get hash to hex
+block = header_hex.decode('hex')
+hash = sha256(sha256(block).digest()).digest()
+block_hash = hash[::-1].encode('hex_codec')
 
-print(header_hex.decode('hex'))
+blockhash = p.getblockhash(nextblockheight)
+block = p.getblock(blockhash)
+
+if(block['previousblockhash'] == block_hash):
+        print("Hashes match! Hash: " + block_hash)
+else:
+        print("Blocks hashes don't match")
